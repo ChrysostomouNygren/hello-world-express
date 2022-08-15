c23bd3665c3ae115e824081295a12a6926ae9400
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-08-15 10:42:56 +0200

    trial, possibly error

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 4000;
+const GREETING = process.env.DUM_TEXT
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hillo World!");
+  res.send(GREETING);
 });
 
 app.listen(PORT, () => {
